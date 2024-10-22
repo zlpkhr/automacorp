6ffb7e4d9fc3bd2fcdbde8f04e3af3bcaf53f06d
Azamat Zulpykhar <azamat_zulpykhar@icloud.com>
2024-10-22 23:59:33 +0200

    Added config bean

--- src/main/java/com/emse/spring/automacorp/AutomacorpApplicationConfig.java
+++ src/main/java/com/emse/spring/automacorp/AutomacorpApplicationConfig.java
@@ -4,6 +4,7 @@ import com.emse.spring.automacorp.hello.GreetingService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AutomacorpApplicationConfig {
@@ -11,5 +12,10 @@ public class AutomacorpApplicationConfig {
     @Bean
     public CommandLineRunner greetingCommandLine(GreetingService greetingService) {
         return args -> greetingService.greet("Spring");
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
