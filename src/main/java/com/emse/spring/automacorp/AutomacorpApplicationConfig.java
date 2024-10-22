6266b9a3faac867471ce136f0e5e50ef28e1c088
Azamat Zulpykhar <azamat_zulpykhar@icloud.com>
2024-10-23 00:06:16 +0200

    Removed rest template bean

--- src/main/java/com/emse/spring/automacorp/AutomacorpApplicationConfig.java
+++ src/main/java/com/emse/spring/automacorp/AutomacorpApplicationConfig.java
@@ -4,7 +4,6 @@ import com.emse.spring.automacorp.hello.GreetingService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AutomacorpApplicationConfig {
@@ -12,10 +11,5 @@ public class AutomacorpApplicationConfig {
     @Bean
     public CommandLineRunner greetingCommandLine(GreetingService greetingService) {
         return args -> greetingService.greet("Spring");
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 }
